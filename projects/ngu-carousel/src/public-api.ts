5d8f9b3585872d4801944399b7262ad8a0b3678e
Santosh Yadav <santosh.yadav198613@gmail.com>
2020-03-15 13:42:48 +0530

    build fixes

--- projects/ngu-carousel/src/public-api.ts
+++ projects/ngu-carousel/src/public-api.ts
@@ -9,3 +9,13 @@ export { NguCarouselStore } from './lib/ngu-carousel/ngu-carousel';
 export { NguCarousel } from './lib/ngu-carousel/ngu-carousel.component';
 
 export { NguCarouselModule } from './lib/ngu-carousel.module';
+
+export { NguItemComponent } from './lib/ngu-item/ngu-item.component';
+
+export { NguTileComponent } from './lib/ngu-tile/ngu-tile.component';
+
+export {
+  NguCarouselDefDirective, NguCarouselPointDirective,
+  NguCarouselItemDirective, NguCarouselNextDirective, NguCarouselPrevDirective,
+  NguCarouselOutlet
+} from './lib/ngu-carousel.directive';
