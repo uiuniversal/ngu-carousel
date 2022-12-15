fd24c4795f9bae73705346deecb6c39b63794480
Brandon Largeau <Yberion@users.noreply.github.com>
2022-12-15 20:21:51 +0100

    chore: update to Angular 15 (#398)
    * chore: update to latest Angular 14 version
    
    * chore: update all packages
    
    * chore: update to Angular 15
    
    * feat(builder): use esbuild
    
    * chore(lib): update tslib
    
    * chore: update eslint and re-generate yarn.lock
    
    Co-authored-by: Brandon <brandon.largeau@sii.fr>

--- projects/ngu-carousel/src/test.ts
+++ projects/ngu-carousel/src/test.ts
@@ -9,13 +9,7 @@ import 'core-js/es7/reflect';
 import 'zone.js/dist/zone';
 import 'zone.js/dist/zone-testing';
 
-declare const require: any;
-
-// First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
-  teardown: { destroyAfterEach: false }
+  errorOnUnknownElements: true,
+  errorOnUnknownProperties: true
 });
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,8 @@
 import { LayoutModule } from '@angular/cdk/layout';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { MatButtonModule } from '@angular/material/button';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
+import { MatLegacyListModule as MatListModule } from '@angular/material/legacy-list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
