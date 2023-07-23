b442fda2c013b38c08842f43a9141bb534cbaf4a
Santosh Yadav <santosh.yadav198613@gmail.com>
2023-07-23 11:29:01 +0000

    fix build

--- apps/ngu-carousel-example/src/app/wrapped/wrapped.module.ts
+++ apps/ngu-carousel-example/src/app/wrapped/wrapped.module.ts
@@ -1,13 +1,30 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { NguCarouselModule } from '@ngu/carousel';
+import {
+  NguCarousel,
+  NguCarouselDefDirective,
+  NguCarouselNextDirective,
+  NguCarouselPrevDirective,
+  NguItemComponent,
+  NguTileComponent
+} from '@ngu/carousel';
 import { WrappedComponent } from './wrapped.component';
 import { WrappedCarouselComponent } from './wrapped-carousel/wrapped-carousel.component';
 import { WrappedRoutingModule } from './wrapped-routing.module';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
-  imports: [WrappedRoutingModule, CommonModule, NguCarouselModule, HttpClientModule],
+  imports: [
+    WrappedRoutingModule,
+    CommonModule,
+    NguCarousel,
+    NguCarouselDefDirective,
+    NguCarouselNextDirective,
+    NguCarouselPrevDirective,
+    NguItemComponent,
+    NguTileComponent,
+    HttpClientModule
+  ],
   exports: [],
   declarations: [WrappedComponent, WrappedCarouselComponent],
   providers: []
