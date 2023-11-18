811c78f61435d6701a6743aa1d4ce79b1b558dba
Yberion <brandon.largeau@gmail.com>
2023-11-18 16:03:20 +0100

    test(ngu-carousel): polyfills cleanup

--- libs/ngu/carousel/src/test.ts
+++ libs/ngu/carousel/src/test.ts
@@ -5,8 +5,6 @@ import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
-import 'zone.js';
-import 'zone.js/testing';
 
 getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
   errorOnUnknownElements: true,
