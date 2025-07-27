429e2b9ceec2723693534cf293c4897a6375cc7b
Santosh Yadav <santosh.yadav198613@gmail.com>
2025-07-27 18:41:12 +0200

    angular 20

--- apps/ngu-carousel-example/src/app/app.config.ts
+++ apps/ngu-carousel-example/src/app/app.config.ts
@@ -1,12 +1,8 @@
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { APP_ROUTES } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(APP_ROUTES),
-    provideAnimations(),
-    provideExperimentalZonelessChangeDetection()
-  ]
+  providers: [provideRouter(APP_ROUTES), provideAnimations(), provideZonelessChangeDetection()]
 };
